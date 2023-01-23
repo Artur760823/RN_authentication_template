07ee5cd97eef019bd4dac56e9a3e826a2a40392c
Artur <artur.zajaczkowski76@gmail.com>
2023-01-23 08:58:48 +0100

    fix

--- util/auth.js
+++ util/auth.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyBrDBbqp_CVBfc5Dbfr8GfCLVepFoDZxJA';
+const API_KEY = '0000000000000000000000000000000000000000000000';
 
 async function authenticate(mode, email, password){
-    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+    const url = `00000000000000000000000000000000000000000000:${mode}?key=${API_KEY}`;
 
     const response = await axios.post(url,
         {
